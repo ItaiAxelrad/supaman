28c454a195901b6ca61bbac96f402853c9d2852a
ItaiAxelrad <itaiaxelrad@gmail.com>
2025-03-21 09:22:04 -0600

    fix: rm workflows

--- components/ui/ColorSchemeToggle.tsx
+++ components/ui/ColorSchemeToggle.tsx
@@ -1,41 +1,41 @@
-"use client";
+'use client';
 
 import {
   ActionIcon,
   Tooltip,
   useComputedColorScheme,
   useMantineColorScheme,
-} from "@mantine/core";
-import { IconMoon, IconSun } from "@tabler/icons-react";
+} from '@mantine/core';
+import { IconMoonStars, IconSun } from '@tabler/icons-react';
 
 export default function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme("light", {
+  const computedColorScheme = useComputedColorScheme('light', {
     getInitialValueInEffect: true,
   });
 
   return (
     <ActionIcon
       onClick={() =>
-        setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')
       }
-      variant="default"
-      size="lg"
-      aria-label="Toggle color scheme"
+      variant='default'
+      size='lg'
+      aria-label='Toggle color scheme'
     >
-      {computedColorScheme === "dark" ? (
-        <Tooltip label="Light mode" offset={4}>
+      {computedColorScheme === 'dark' ? (
+        <Tooltip label='Light mode' offset={4}>
           <IconSun
-            color="var(--mantine-color-yellow-6)"
-            fill="var(--mantine-color-yellow-light)"
+            color='var(--mantine-color-yellow-6)'
+            fill='var(--mantine-color-yellow-light)'
             stroke={1.5}
           />
         </Tooltip>
       ) : (
-        <Tooltip label="Dark mode" offset={4}>
-          <IconMoon
-            color="var(--mantine-color-indigo-6)"
-            fill="var(--mantine-color-indigo-light)"
+        <Tooltip label='Dark mode' offset={4}>
+          <IconMoonStars
+            color='var(--mantine-color-indigo-6)'
+            fill='var(--mantine-color-indigo-light)'
             stroke={1.5}
           />
         </Tooltip>
